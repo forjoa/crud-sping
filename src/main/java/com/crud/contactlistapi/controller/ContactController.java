79f46201ddbb9bc45c57048522a715e68cfe80f2
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-07-08 00:17:49 +0200

    build: create contact endpoint

--- src/main/java/com/crud/contactlistapi/controller/ContactController.java
+++ src/main/java/com/crud/contactlistapi/controller/ContactController.java
@@ -3,10 +3,7 @@ package com.crud.contactlistapi.controller;
 import com.crud.contactlistapi.entity.Contact;
 import com.crud.contactlistapi.repositories.ContactRespository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/contacts")
 @RestController
@@ -22,5 +19,10 @@ public class ContactController {
     @GetMapping("{id}")
     public Contact get(@PathVariable Integer id) {
         return contactRespository.findById(id).orElse(null);
+    }
+
+    @PostMapping
+    public Contact create(@RequestBody Contact contact) {
+        return contactRespository.save(contact);
     }
 }
