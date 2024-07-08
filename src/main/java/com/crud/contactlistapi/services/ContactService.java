c247b8e8087a9f68d5fd8f68e799277523aa054d
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-07-08 17:35:41 +0200

    build: create contact service

---
+++ src/main/java/com/crud/contactlistapi/services/ContactService.java
@@ -0,0 +1,44 @@
+package com.crud.contactlistapi.services;
+
+import com.crud.contactlistapi.entity.Contact;
+import com.crud.contactlistapi.repositories.ContactRespository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.time.LocalDateTime;
+
+@Service
+public class ContactService {
+
+    @Autowired
+    private ContactRespository contactRespository;
+
+    public Iterable<Contact> findAll() {
+        return contactRespository.findAll();
+    }
+
+    public Contact get(Integer id) {
+        return contactRespository.findById(id).orElse(null);
+    }
+
+    public Contact create(Contact contact) {
+        contact.setCreatedAt(LocalDateTime.now());
+        return contactRespository.save(contact);
+    }
+
+    public Contact update(Integer id,Contact contact) {
+        Contact contactFromDb = contactRespository.findById(id).orElse(null);
+
+        contactFromDb.setName(contact.getName());
+        contactFromDb.setEmail(contact.getEmail());
+
+        return contactRespository.save(contactFromDb);
+    }
+
+    public void delete (Integer id) {
+        Contact contactFromDb = contactRespository.findById(id).orElse(null);
+        contactRespository.delete(contactFromDb);
+    }
+}
